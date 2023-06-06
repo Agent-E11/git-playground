eb75e2a223ac8f103216751c93b8dfaabcc82cb2
Eamon <eamon.r.burns@gmail.com>
2023-06-05 21:15:39 -0700

    - Added comments - I also want to test if this message will successfully mention issue #1

--- aScript.py
+++ aScript.py
@@ -1,10 +1,13 @@
-
-
+# Introduce yourself
 print('Hello, this is a script.')
+
+# Ask a random question
 text = input('What would you like to say?: ')
 
+# Respond to their question
 print('\nThis is the hexadecimal value of what you just input:')
 print(text.encode().hex('-'))
 
+# Respond again
 print('\nAnd this is binary:')
 print(bin(int(text.encode().hex(), base=16)))
